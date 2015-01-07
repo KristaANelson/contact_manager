b8be10a9ac0f8f8c58efb12d71872bfc91fac00c
KristaANelson <krista.a.nelson@gmail.com>
2015-01-07 00:13:46 -0700

    Gernerate phone number and associate with person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -9,14 +9,18 @@ RSpec.describe Person, :type => :model do
     expect(person).to be_valid
   end
 
- it 'is invalid without a first name' do
-   person.first_name = nil
-   person = Person.new(first_name: nil)
-   expect(person).not_to be_valid
- end
+  it 'is invalid without a first name' do
+    person.first_name = nil
+    person = Person.new(first_name: nil)
+    expect(person).not_to be_valid
+  end
 
- it 'is invalid without a last name' do
-   person = Person.new(first_name: 'Bob', last_name: nil)
-   expect(person).not_to be_valid
- end
+  it 'is invalid without a last name' do
+    person = Person.new(first_name: 'Bob', last_name: nil)
+    expect(person).not_to be_valid
+  end
+
+  it 'has an array of phone numbers' do
+    expect(person.phone_numbers).to eq([])
+  end
 end
